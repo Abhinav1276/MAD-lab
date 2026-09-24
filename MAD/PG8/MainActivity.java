e5c30099d252c02ef86f6ae0aaaa2cf1f7c39ff9
Abhinav1276 <abhinavk1276@gmail.com>
2026-09-24 15:37:05 +0530

    Add files via upload

---
+++ MAD/PG8/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.pg8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView iv;
+    Button btn;
+
+    int images[] = {
+            R.drawable.first, R.drawable.second
+    };
+
+    int currentindex = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        iv = findViewById(R.id.imgv1);
+        btn = findViewById(R.id.btn);
+    }
+
+    public void Click(View view) {
+
+        if (currentindex == 0) {
+            iv.setImageResource(images[1]);
+            currentindex = 1;
+
+        } else {
+            iv.setImageResource(images[0]);
+            currentindex = 0;
+        }
+    }
+}
+
